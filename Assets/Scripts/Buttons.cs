f0a5f49753d5a41212ff9526011606df4f789e90
bennie12345 <bmuijldert@gmail.com>
2015-09-01 12:39:55 +0200

    First Commit

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        AutoFade.LoadLevel("Game", 3, 1, Color.black);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void MainMenu() 
+    {
+        Application.LoadLevel("Menu");
+    }
+}
---
+++ Assets/Scripts/Intro_script.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro_script : MonoBehaviour {
+
+	// Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Example());
+    }
+
+    IEnumerator Example()
+    {
+        yield return new WaitForSeconds(2);
+        AutoFade.LoadLevel("Menu",3,1,Color.black);
+        print("ayy");
+    }
+
+    #if UNITY_EDITOR
+        void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Application.LoadLevel("Menu");
+            }
+        }
+    #endif
+}
